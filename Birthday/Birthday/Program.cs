4484a6e69ff8edf09be9cbf6e4ffde8ca70c8b60
Unknown <19liukat@elmbrookstudents.org>
2018-06-13 11:37:21 -0500

    birthday wishes

--- Birthday/Birthday/Program.cs
+++ Birthday/Birthday/Program.cs
@@ -7,6 +7,7 @@ namespace Birthday
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("A dog");
 			Console.ReadKey();
         }
     }
